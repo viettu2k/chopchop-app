68b69d2d3ca8ee1e2672ac233dd097b484dcac68
your_usernameviettu2k <doanviettu2k@gmail.com>
2022-07-11 11:01:26 +0700

    adding schema

---
+++ chopchop-backend/schemas/user.js
@@ -0,0 +1,9 @@
+export default {
+    name: "user",
+    title: "User",
+    type: "document",
+    fields: [
+        { name: "userName", title: "User Name", type: "string" },
+        { name: "iamge", title: "Image", type: "string" },
+    ],
+};
