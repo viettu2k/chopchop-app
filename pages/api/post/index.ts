b2815cc6ffe00f9ac35d6cfc586a41ce13ed7d8f
your_usernameviettu2k <doanviettu2k@gmail.com>
2022-07-12 09:23:42 +0700

    adding MainVideo Component

---
+++ pages/api/post/index.ts
@@ -0,0 +1,17 @@
+import { allPostsQuery } from "./../../../utils/queries";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { client } from "../../../utils/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.status(200).json({ name: "Response successful" });
+  if (req.method === "GET") {
+    const query = allPostsQuery();
+
+    const data = await client.fetch(query);
+
+    res.status(200).json(data);
+  }
+}
