7802593224719e6590525bc55573a21b3e25a6a0
your_usernameviettu2k <doanviettu2k@gmail.com>
2022-07-13 10:50:14 +0700

    setup detail page

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -67,7 +67,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
           onMouseLeave={() => setIsHover(false)}
           className="rounded-3xl"
         >
-          <Link href="/">
+          <Link href={`/detail/${post._id}`}>
             <video
               ref={videoRef}
               className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
