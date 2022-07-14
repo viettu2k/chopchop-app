4f2c32ec3522e00fc6f40445972187f025e0a7cf
your_usernameviettu2k <doanviettu2k@gmail.com>
2022-07-14 14:24:46 +0700

    Updating Comments component

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -5,6 +5,7 @@ import { GoVerified } from "react-icons/go";
 
 import useAuthStore from "../store/authStore";
 import NoResults from "./NoResults";
+import { IUser } from "../types";
 
 interface IProps {
   isPostingComment: Boolean;
@@ -28,13 +29,49 @@ const Comments = ({
   comments,
   isPostingComment,
 }: IProps) => {
-  const { userProfile }: any = useAuthStore();
+  const { userProfile, allUsers }: any = useAuthStore();
 
   return (
     <div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]">
       <div className="overflow-scroll lg:h-[475px]">
         {comments?.length ? (
-          <div>videos</div>
+          comments.map((item, index) => (
+            <>
+              {allUsers.map(
+                (user: IUser) =>
+                  user._id === (item.postedBy._id || item.postedBy._ref) && (
+                    <div className="p-2 items-center" key={index}>
+                      <Link href={`/profile/${user._id}`}>
+                        <div className="flex items-start gap-3">
+                          <div className="w-8 h-8">
+                            <Image
+                              src={user.image}
+                              width={34}
+                              height={34}
+                              alt="user profile"
+                              className="rounded-full"
+                            />
+                          </div>
+
+                          <div className="hidden xl:block">
+                            <p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
+                              {user.userName.replaceAll(" ", "")}
+                            </p>
+                            <GoVerified className="text-blue-400" />
+                            <p className="capitalize text-gray-400 text-xs">
+                              {user.userName}
+                            </p>
+                          </div>
+                        </div>
+                      </Link>
+                      <div>
+                        <p>{item.comment}</p>
+                      </div>
+                    </div>
+                  )
+              )}
+            </>
+          ))
         ) : (
           <NoResults text="No comments yet! Be the first one to add a comment." />
         )}
@@ -50,7 +87,10 @@ const Comments = ({
               placeholder="Add comment..."
               className="bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:boder-2 focus:border-gray-300 flex-1 rounded-lg"
             />
-            <button className="text-md text-gray-400" onClick={addComment}>
+            <button
+              className="inline-block px-8 py-3 text-sm font-medium text-white transition bg-indigo-600 rounded hover:scale-110 hover:shadow-xl active:bg-indigo-500 focus:outline-none focus:ring"
+              onClick={addComment}
+            >
               {isPostingComment ? "Comenting..." : "Comment"}
             </button>
           </form>
